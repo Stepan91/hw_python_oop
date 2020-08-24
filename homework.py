fe669eef738115060626b56cbe5327fe9d5115e9
Stepan Pryn <prynstepan@yandex.ru>
2020-08-25 01:23:35 +0300

    4nd

--- homework.py
+++ homework.py
@@ -17,22 +17,27 @@ class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
+        self.today = dt.date.today()
 
     def add_record(self, record):
         self.records.append(record)
 
+    # метод-то я создал, а вот как его применить - не знаю
+    # без создания цикла в функциях-счетчиках нельзя пройтись по датам
+    def sum_amounts(self):
+        sum(record_i.amount
+            for record_i in self.records)
+
     def get_today_stats(self):
-        today = dt.date.today()
-        sum_records = sum(int(record_i.amount)
-                          for record_i in self.records if
-                          record_i.date == today)
-        return sum_records
+        return sum(record_i.amount
+                   for record_i in self.records if
+                   record_i.date == self.today)
 
     def get_week_stats(self):
         date_week = dt.date.today() - dt.timedelta(days=6)
-        sum_records = sum(int(record_i.amount)
+        sum_records = sum(record_i.amount
                           for record_i in self.records if
-                          dt.date.today() >= record_i.date >= date_week)
+                          self.today >= record_i.date >= date_week)
         return sum_records
 
     def remainder_on(self):
@@ -40,44 +45,24 @@ class Calculator:
 
 
 class CashCalculator(Calculator):
-    EURO_RATE = float(80)
-    USD_RATE = float(70)
+    EURO_RATE = float(70)
+    USD_RATE = float(60)
 
     def get_today_cash_remained(self, currency):
         remainder = self.remainder_on()
-        remainder_eur = remainder/self.EURO_RATE
-        remainder_usd = remainder/self.USD_RATE
-        abs_remainder = abs(remainder)
-        abs_remainder_eur = abs(remainder_eur)
-        abs_remainder_usd = abs(remainder_usd)
         rates = {
-            "rub": "руб",
-            "eur": "Euro",
-            "usd": "USD"
+            "rub": (1, "руб"),
+            "eur": (self.EURO_RATE, "Euro"),
+            "usd": (self.USD_RATE, "USD")
             }
-        rate = rates[currency]
-
         if remainder > 0:
-            if currency == "rub":
-                return f"На сегодня осталось {remainder:.2f} {rate}"
-            elif currency == "eur":
-                return ("На сегодня осталось"
-                        f" {remainder_eur:.2f} {rate}")
-            else:
-                return ("На сегодня осталось"
-                        f" {remainder_usd:.2f} {rate}")
+            remainder = remainder/rates[currency][0]
+            return f"На сегодня осталось {remainder:.2f} {rates[currency][1]}"
         elif remainder == 0:
             return ("Денег нет, держись")
-        else:
-            if currency == "rub":
-                return ("Денег нет, держись: твой долг -"
-                        f" {abs_remainder:.2f} {rate}")
-            elif currency == "eur":
-                return ("Денег нет, держись: твой долг -"
-                        f" {abs_remainder_eur:.2f} {rate}")
-            else:
-                return ("Денег нет, держись: твой долг -"
-                        f" {abs_remainder_usd:.2f} {rate}")
+        remainder = abs(remainder/rates[currency][0])
+        return ("Денег нет, держись: твой долг -"
+                f" {remainder:.2f} {rates[currency][1]}")
 
 
 class CaloriesCalculator(Calculator):
@@ -87,8 +72,7 @@ class CaloriesCalculator(Calculator):
         if remainder_cal > 0:
             return ("Сегодня можно съесть что-нибудь ещё, "
                     f"но с общей калорийностью не более {remainder_cal} кКал")
-        else:
-            return "Хватит есть!"
+        return "Хватит есть!"
 
 
 if __name__ == "__main__":
